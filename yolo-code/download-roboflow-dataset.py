fa75ec2b7264d2fa631aad245c71774072da7eda
Jason Pien <jasonpien@Jasons-MacBook-Pro-27993.local>
2024-05-06 17:11:47 -0400

    dataset downlaoder

---
+++ yolo-code/download-roboflow-dataset.py
@@ -0,0 +1,8 @@
+from roboflow import Roboflow
+rf = Roboflow(api_key = 'a9VkdJpM9YHFBOsfIzbB', model_format="yolov5", notebook="roboflow-yolov5")
+rf = Roboflow(api_key="a9VkdJpM9YHFBOsfIzbB")
+project = rf.workspace("karel-cornelis-q2qqg").project("aicook-lcv4d")
+version = project.version(4)
+dataset = version.download("yolov5")
+
+#Just run this file
